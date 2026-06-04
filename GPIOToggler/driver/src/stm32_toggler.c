6f7cc82f7a9c4a181e9be116047b78226438296f
Chitransh Damedhar <chitranshdamedhar@gmail.com>
2026-06-05 03:21:05 +0530

    Fix GPIO driver implementation: correct function names, add missing implementations for write/toggle, improve comments and use RCC_AHB1RSTR for reset

--- GPIOToggler/driver/src/stm32_toggler.c
+++ GPIOToggler/driver/src/stm32_toggler.c
@@ -9,6 +9,12 @@
 
 #include <stdint.h>
 #include <stdlib.h>
+
+/**
+ * @brief Enable or disable GPIO peripheral clock
+ * @param pGPIOx Pointer to GPIO port
+ * @param EnorDi ENABLE or DISABLE
+ */
 void GPIO_PeriClockControl(GPIO_regdef_t *pGPIOx, uint8_t EnorDi){
     if(EnorDi == ENABLE){
         if(pGPIOx == GPIOA) RCC->RCC_AHB1ENR |= (1 << 0);
@@ -28,10 +34,11 @@ void GPIO_PeriClockControl(GPIO_regdef_t *pGPIOx, uint8_t EnorDi){
     }
 }
 
-
-
-
-void GPIO_Inti(GPIO_Handle_t *pGPIOHandle){
+/**
+ * @brief Initialize GPIO pin with specified configuration
+ * @param pGPIOHandle Pointer to GPIO handle containing port and configuration
+ */
+void GPIO_Init(GPIO_Handle_t *pGPIOHandle){
 	uint32_t temp = 0;
 
 	if(pGPIOHandle->GPIO_PinConfig.GPIO_PinMode <= GPIO_MODE_ANALOG)
@@ -66,40 +73,47 @@ void GPIO_Inti(GPIO_Handle_t *pGPIOHandle){
 	    pGPIOHandle->pGPIOx->AFR[afr_index] &= ~(0xF << (4 * afr_pos));
 	    pGPIOHandle->pGPIOx->AFR[afr_index] |= (pGPIOHandle->GPIO_PinConfig.GPIO_PinAltFunMode << (4 * afr_pos));
 	}
-
 }
-void GPIO_DeInti(GPIO_regdef_t *pGPIOx)
+
+/**
+ * @brief De-initialize GPIO port (reset to default state)
+ * @param pGPIOx Pointer to GPIO port
+ */
+void GPIO_DeInit(GPIO_regdef_t *pGPIOx)
 {
 	if(pGPIOx==GPIOA)
 	{
-		RCC->RCC_AHB1ENR |=(1<<0);
-		RCC->RCC_AHB1ENR &=~(1<<0);
+		RCC->RCC_AHB1RSTR |=(1<<0);
+		RCC->RCC_AHB1RSTR &=~(1<<0);
 	}
 	else if(pGPIOx==GPIOB){
-		RCC->RCC_AHB1ENR |=(1<<1);
-		RCC->RCC_AHB1ENR &=~(1<<1);
+		RCC->RCC_AHB1RSTR |=(1<<1);
+		RCC->RCC_AHB1RSTR &=~(1<<1);
 	}
 	else if(pGPIOx==GPIOC){
-			RCC->RCC_AHB1ENR |=(1<<2);
-			RCC->RCC_AHB1ENR &=~(1<<2);
-		}
+		RCC->RCC_AHB1RSTR |=(1<<2);
+		RCC->RCC_AHB1RSTR &=~(1<<2);
+	}
 	else if(pGPIOx==GPIOD){
-			RCC->RCC_AHB1ENR |=(1<<3);
-			RCC->RCC_AHB1ENR&=~(1<<3);
-		}
+		RCC->RCC_AHB1RSTR |=(1<<3);
+		RCC->RCC_AHB1RSTR &=~(1<<3);
+	}
 	else if(pGPIOx==GPIOE){
-			RCC->RCC_AHB1ENR |=(1<<4);
-			RCC->RCC_AHB1ENR
-			&=~(1<<4);
-
-		}
+		RCC->RCC_AHB1RSTR |=(1<<4);
+		RCC->RCC_AHB1RSTR &=~(1<<4);
+	}
 	else if(pGPIOx==GPIOH){
-			RCC->RCC_AHB1ENR|=(1<<7);
-			RCC->RCC_AHB1ENR&=~(1<<7);
-
-		}
-
+		RCC->RCC_AHB1RSTR |=(1<<7);
+		RCC->RCC_AHB1RSTR &=~(1<<7);
+	}
 }
+
+/**
+ * @brief Read GPIO pin value
+ * @param pGPIOx Pointer to GPIO port
+ * @param PinNumber Pin number to read (0-15)
+ * @return 0 or 1
+ */
 uint8_t GPIO_ReadFromInputPin(GPIO_regdef_t *pGPIOx, uint8_t PinNumber)
 {
     uint8_t value;
@@ -109,6 +123,12 @@ uint8_t GPIO_ReadFromInputPin(GPIO_regdef_t *pGPIOx, uint8_t PinNumber)
 
     return value;
 }
+
+/**
+ * @brief Read entire GPIO port
+ * @param pGPIOx Pointer to GPIO port
+ * @return 16-bit port value
+ */
 uint16_t GPIO_ReadFromInputPort(GPIO_regdef_t *pGPIOx)
 {
     uint16_t value;
@@ -119,8 +139,40 @@ uint16_t GPIO_ReadFromInputPort(GPIO_regdef_t *pGPIOx)
     return value;
 }
 
+/**
+ * @brief Write value to GPIO pin
+ * @param pGPIOx Pointer to GPIO port
+ * @param PinNumber Pin number (0-15)
+ * @param Value Value to write (0 or 1)
+ */
+void GPIO_WriteToOutputPin(GPIO_regdef_t *pGPIOx, uint8_t PinNumber, uint8_t Value)
+{
+    if(Value == GPIO_PIN_SET)
+    {
+        pGPIOx->ODR |= (1 << PinNumber);
+    }
+    else
+    {
+        pGPIOx->ODR &= ~(1 << PinNumber);
+    }
+}
 
+/**
+ * @brief Write value to entire GPIO port
+ * @param pGPIOx Pointer to GPIO port
+ * @param Value 16-bit value to write
+ */
+void GPIO_WriteToOutputPort(GPIO_regdef_t *pGPIOx, uint16_t Value)
+{
+    pGPIOx->ODR = Value;
+}
 
-
-
-
+/**
+ * @brief Toggle GPIO pin state
+ * @param pGPIOx Pointer to GPIO port
+ * @param PinNumber Pin number (0-15)
+ */
+void GPIO_ToggleOutputPin(GPIO_regdef_t *pGPIOx, uint8_t PinNumber)
+{
+    pGPIOx->ODR ^= (1 << PinNumber);
+}
